77d4ca21bc2a0e8da9d75d159f1c303b2454839e
WtfMontag <fahrenheit843f@gmail.com>
2022-09-09 09:58:56 +0300

    leetCode

---
+++ LeetCode/0344_Reverse_String/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _0344_Reverse_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            char[] s = { 'h', 'e', 'l', 'l', 'o' };
+            for (int i = 0, j = s.Length - 1; i < s.Length / 2; i++, j--)
+            {
+                char temp = s[i];
+                s[i] = s[j];
+                s[j] = temp;
+            }
+            */
+        }
+    }
+}
---
+++ LeetCode/0557_Reverse_Words_in_a_String_III/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _0557_Reverse_Words_in_a_String_III
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Let's take LeetCode contest";
+            string[] a = s.Split();
+            string answer = String.Empty;
+            for (int i = 0; i < a.Length; i++)
+            {
+                char[] ss = a[i].ToCharArray();
+                Array.Reverse(ss);
+                s = new string(ss);
+                answer += s + " ";
+            }
+            Console.WriteLine(answer.Trim());
+        }
+    }
+}
