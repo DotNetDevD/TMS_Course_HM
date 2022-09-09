4cf7ac47f9a0e79b69c9a0d343f78277affcb6c3
WtfMontag <fahrenheit843f@gmail.com>
2022-09-09 13:11:25 +0300

    add Task1_TimerPractice

---
+++ TMS_10/Task_01_TimerPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿// Изучить класс Timer
+// Написать код с использованием класса Timer
+
+using System.Diagnostics;
+using Task_01_TimerPractice;
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+SomeAction someAction = new(5000, 5000, stopwatch);
+
+TimerCallback tcb = DoSomeAction;
+
+Timer timer = new Timer(tcb, someAction, someAction.startTime, someAction.periodOfTime);
+
+Console.ReadLine();
+
+static void DoSomeAction(object someAction)
+{
+    var action = (SomeAction)someAction;
+    Console.WriteLine($"This action will be made every {action.periodOfTime / 1000} sec...");
+    Console.WriteLine($"Time passed: {action.stopwatch.Elapsed.TotalSeconds:F2}");
+    action.stopwatch.Restart();
+}
+
+
---
+++ TMS_10/Task_01_TimerPractice/SomeAction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01_TimerPractice
+{
+   public struct SomeAction
+    {
+        public int startTime;
+        public int periodOfTime;
+        public Stopwatch stopwatch;
+        public SomeAction(int startTime, int periodOfTime, Stopwatch stopwatch)
+        {
+            this.startTime = startTime;
+            this.periodOfTime = periodOfTime;
+            this.stopwatch = stopwatch;
+        }
+    }
+
+}
