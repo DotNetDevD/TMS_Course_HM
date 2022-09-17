8c54589889285b971cb2f022c57933f020038f6e
WtfMontag <fahrenheit843f@gmail.com>
2022-09-17 15:30:07 +0300

    add task 2

--- TMS_11/Task_02_Serialize/Program.cs
+++ TMS_11/Task_02_Serialize/Program.cs
@@ -1,4 +1,152 @@
-﻿
+﻿using Task_02_Serialize;
+using System.Xml.Serialization;
+using System.Text.Json;
+
+Point squarePoint = new(10, 10);
+Shape square = new(squarePoint, 100, 100, "Square");
+Shape rectangle = new(new Point(20, 20), 100, 200, "Rectangle");
+Shape romb = new(new Point(30, 30), 50, 50, "Romb");
+
+Shape[] shapes = new Shape[] { square, rectangle, romb };
+XmlSerializer xmlSerializer = new XmlSerializer(typeof(Shape[]));
+
+Console.WriteLine("Введите название файла для Xml сериализации: ");
+string pathXmlSerializeName = CorrectFileName();
+XmlSerialize(pathXmlSerializeName, xmlSerializer, shapes);
+
+Console.WriteLine("Введите название файла для Xml десериализации: ");
+Shape[]? newXmlShapes = isXmlDeserialize(pathXmlSerializeName, xmlSerializer, shapes);
+ShowShapes(newXmlShapes);
+
+Console.WriteLine(new string('-', 45));
+
+Console.WriteLine("Введите название файла для Json сериализации: ");
+string pathJsonSerializeName = CorrectFileName();
+JsonSerialize(pathJsonSerializeName, shapes);
+
+Console.WriteLine("Введите название файла для Json десериализации: ");
+Shape[]? newJsonShapes = isJsonDeserialize(pathJsonSerializeName, shapes);
+ShowShapes(newJsonShapes);
+
+
+static void ShowShapes(Shape[] shapes)
+{
+    if (shapes != null)
+    {
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Name: {shape.Name} --- Length: {shape.Length} --- Height: {shape.Height}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Список пуст");
+    }
+}
+
+static string CorrectFileName()
+{
+    string? fileName = Console.ReadLine();
+    string sourceFolder = @"D:\";
+    if (!string.IsNullOrEmpty(fileName) && fileName.IndexOfAny(Path.GetInvalidFileNameChars()) < 0 &&
+              !File.Exists(Path.Combine(sourceFolder, fileName)))
+    {
+        return fileName;
+    }
+    else
+    {
+        throw new Exception("Неверный ввод имя файла");
+    }
+}
+
+static void XmlSerialize(string pathName, XmlSerializer xmlSerializer, Shape[] shapes)
+{
+    string xmlPathSerializer = @$"D:\{pathName}.xml";
+    using (FileStream fs = new FileStream(xmlPathSerializer, FileMode.OpenOrCreate))
+    {
+        xmlSerializer.Serialize(fs, shapes);
+    }
+    Console.WriteLine("Object has been XML serialized");
+}
+
+static Shape[] XmlDeserialize(string pathName, XmlSerializer xmlSerializer, Shape[] shapes)
+{
+    string xmlPathDeserializer = $@"D:\{pathName}.xml";
+    using (FileStream fs = new FileStream(xmlPathDeserializer, FileMode.OpenOrCreate))
+    {
+        shapes = xmlSerializer.Deserialize(fs) as Shape[];
+    }
+    Console.WriteLine("XML Deserialize");
+
+    return shapes;
+}
+
+static Shape[] isXmlDeserialize(string pathXmlSerializeName, XmlSerializer xmlSerializer, Shape[] shapes)
+{
+    bool isDeserialize = true;
+    Shape[]? newXmlShapes = null;
+    do
+    {
+        try
+        {
+            string pathName = CorrectFileName();
+            newXmlShapes = XmlDeserialize(pathName, xmlSerializer, shapes);
+            isDeserialize = false;
+        }
+        catch
+        {
+            Console.WriteLine("Невозможно десериализовать данный файл!");
+            Thread.Sleep(1500);
+            Console.WriteLine($"Не унывай! Попробуй название этого файла: {pathXmlSerializeName}");
+            isDeserialize = true;
+        }
+    } while (isDeserialize);
+
+    return newXmlShapes;
+}
+
+static void JsonSerialize(string pathName, Shape[] shapes)
+{
+    string jsonPathSerializer = @$"D:\{pathName}.json";
+    using (FileStream fs = new FileStream(jsonPathSerializer, FileMode.OpenOrCreate))
+    {
+        JsonSerializer.Serialize(fs, shapes);
+    }
+    Console.WriteLine("Object has been JSON serialized");
+}
+
+static Shape[] JsonDeserialize(string pathName, Shape[] shapes)
+{
+    string jsonPathDeserializer = @$"D:\{pathName}.json";
+    string jsonString = File.ReadAllText(jsonPathDeserializer);
+    shapes = JsonSerializer.Deserialize<Shape[]>(jsonString)!;
+    Console.WriteLine("JSON Deserialize");
+    return shapes;
+}
+
+static Shape[] isJsonDeserialize(string pathJsonSerializeName, Shape[] shapes)
+{
+    Shape[]? newJsonShapes = null;
+    bool isDeserialize = true;
+    do
+    {
+        try
+        {
+            string pathName = CorrectFileName();
+            newJsonShapes = JsonDeserialize(pathName, shapes);
+            isDeserialize = false;
+        }
+        catch
+        {
+            Console.WriteLine("Невозможно десериализовать данный файл!");
+            Thread.Sleep(1500);
+            Console.WriteLine($"Не унывай! Попробуй название этого файла: {pathJsonSerializeName}");
+            isDeserialize = true;
+        }
+
+    } while (isDeserialize);
+    return newJsonShapes;
+}
 
 /*
 3. Написать программу выполняющую сериализацию и десериализацию. Создать класс Point со свойствами float X, float Y. 
---
+++ TMS_11/Task_02_Serialize/Shape.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_02_Serialize
+{
+    public class Shape
+    {
+        public Point ShapePoint { get; set; } = new Point();
+        public float Length { get; set; }
+        public float Height { get; set; }
+        public string Name { get; set; }
+        public Shape() { }
+        public Shape(Point shapePoint, float length, float height, string name)
+        {
+            ShapePoint = shapePoint;
+            Length = length;
+            Height = height;
+            Name = name;
+        }
+    }
+}
