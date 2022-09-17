426c09626957fef7328b3c1425b31533b18451e3
WtfMontag <fahrenheit843f@gmail.com>
2022-09-17 11:39:08 +0300

    add task 1

---
+++ TMS_11/Task_01_Dictionary/ApplicationDictionary.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01_Dictionary
+{
+    internal class ApplicationDictionary<T, V>
+    {
+        private Dictionary<T, V> dictionary;
+
+        public ApplicationDictionary()
+        {
+            dictionary = new Dictionary<T, V>();
+        }
+        public void Add(T key, V value)
+        {
+            dictionary.Add(key, value);
+        }
+        public void Remove(T key)
+        {
+            dictionary.Remove(key, out V? value);
+            Console.WriteLine($"По ключу: <{key}> был удален элемент: <{value}>");
+        }
+        public void ListOfKey()
+        {
+            foreach (var key in dictionary)
+            {
+                Console.WriteLine(key.Key);
+            }
+        }
+        public bool ContainsKey(T key)
+        {
+            return dictionary.ContainsKey(key);
+        }
+        public V GetValueByKey(T key)
+        {
+            if (ContainsKey(key))
+            {
+                dictionary.TryGetValue(key, out V? value);
+                return value;
+            }
+            else
+            {
+                throw new Exception("Данного ключа нет в словаре");
+            }
+        }
+        // Второй вариант получения элемента по ключу через индексатор
+        public string this[T key]
+        {
+            get
+            {
+                string getValue = string.Empty;
+                if (ContainsKey(key))
+                {
+                    dictionary.TryGetValue(key, out V? value);
+                    getValue = $"По ключу: <{key}> был найден элемент: <{value}>";
+                }
+                else
+                {
+                    getValue = $"По ключу: <{key}> элемента в словаре не найдено";
+                }
+                return getValue;
+            }
+        }
+    }
+}
---
+++ TMS_11/Task_01_Dictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using Task_01_Dictionary;
+
+ApplicationDictionary<string, string> dictionary = new ApplicationDictionary<string, string>();
+dictionary.Add("машина", "car");
+dictionary.Add("камень", "stone");
+dictionary.Add("ножницы", "scissors");
+dictionary.Add("бумага", "paper");
+Console.WriteLine(dictionary["машина"]);
+Console.WriteLine(dictionary["..."]);
+dictionary.Remove("машина");
+dictionary.ListOfKey();
+//dictionary.GetValueByKey("машина");
+
+ApplicationDictionary<int, Person> dictionary1 = new ApplicationDictionary<int, Person>();
+Person p1 = new("Valery", 54);
+Person p2 = new("Ibragim", 25);
+Person p3 = new("Masha", 18);
+dictionary1.Add(1, p1);
+dictionary1.Add(2, p2);
+dictionary1.Add(3, p3);
+dictionary1.ListOfKey();
+Person p = dictionary1.GetValueByKey(1);
+Console.WriteLine(p.Name);
+dictionary1.Remove(3);
+Console.WriteLine(dictionary1[3]);
+
+/*
+Реализовать простейший вариант класса Dictionary<TKey, TValue>, например 
+ApplicationDictionary<TKey, TValue>. Класс должен предоставлять возможность добавления элемента в словарь, 
+удаления элемента, получения элемента по ключу, получение списка всех ключей.
+*/
