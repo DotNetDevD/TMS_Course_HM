0e4c332bb02c0572257bcae93a5e093212e10bfa
WtfMontag <fahrenheit843f@gmail.com>
2022-09-18 00:34:09 +0300

    fix bug

--- TMS_11/Task_02_Serialize/NewtonsoftJson.cs
+++ TMS_11/Task_02_Serialize/NewtonsoftJson.cs
@@ -11,14 +11,14 @@ namespace Task_02_Serialize
     {
         public void Serialize(Shape[] shapes, string pathName)
         {
-            string pathNewtonsoftSerializeName = @$"D:\{pathName}.xml";
+            string pathNewtonsoftSerializeName = @$"D:\{pathName}.json";
             string s = JsonConvert.SerializeObject(shapes);
             File.WriteAllText(pathNewtonsoftSerializeName, s);
             Console.WriteLine("Object has been Newtonsoft.Json serialized");
         }
         public Shape[] Deseralize(Shape[] shapes, string pathName)
         {
-            string pathNewtonsoftSerializeName = @$"D:\{pathName}.xml";
+            string pathNewtonsoftSerializeName = @$"D:\{pathName}.json";
             string path = File.ReadAllText(pathNewtonsoftSerializeName);
             Shape[]? newShapes = JsonConvert.DeserializeObject<Shape[]>(path);
             Console.WriteLine("Newtonsoft.Json Deserialize");
